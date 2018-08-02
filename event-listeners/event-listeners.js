0d208841981a65b14511d8f48dbb2f04b9a80a5e
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 16:38:48 +0300

    events added

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,44 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/Cleveland_Cavaliers.jpg', './imgs/Dallas_Mavericks.jpg', './imgs/miami_heat.jpg', './imgs/golden-state.jpg'];
 var currentIndex = 0;
+var pic = document.getElementById("picture");
 
-function showNextPicture() {
+
+pic.addEventListener("click", function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
+  pic.src = pictures[currentIndex];
 
   // YOUR TASK: Finish this function!
-}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,10 @@
 // Put all your JavaScript in this file!
+function changeColor() {
+	var c = askColor();
+	document.getElementsByTagName("body")[0].style.backgroundColor = c;
+
+}
+
+function askColor() {
+	return prompt("Pick a color: ");
+}
